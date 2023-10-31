c9153bdd3ea73d62f5787785325bd114e2ce187d
Katsuya Horiuchi <khoriuchi@lavabit.com>
2023-11-01 01:27:29 +0900

    Make background transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,5 +5,9 @@ return {
   priority = 1000, -- Make sure to load this before all the other start plugins
   config = function()
     vim.cmd([[colorscheme monokai]])
+
+    -- Make background transparent
+    -- Reference: https://github.com/tanvirtin/monokai.nvim/issues/13
+    vim.cmd([[:hi Normal ctermbg=none guibg=none]])
   end,
 }
