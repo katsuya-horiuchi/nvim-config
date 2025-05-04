ea41c9c50866122391e18830a7ac2abef586ec78
Katsuya Horiuchi <khoriuchi@lavabit.com>
2025-05-05 01:18:40 +0900

    Properly configure lsp for lua

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -39,6 +39,31 @@ return {
       },
     })
 
-    lspconfig.lua_ls.setup({})
+    lspconfig.lua_ls.setup({
+      -- This disable "Undefined global vim" error message
+      -- https://github.com/neovim/neovim/discussions/24119#discussioncomment-9137639
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+          return
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          runtime = {
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+            }
+          }
+        })
+      end,
+      settings = {
+        Lua = {}
+      }
+    })
   end,
 }
